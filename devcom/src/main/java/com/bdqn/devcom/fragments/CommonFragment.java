0cb26ad0e4a93151170d67cb9022e9fa010eb000
heshuhui <1052637118@qq.com>
2016-07-03 21:51:50 +0800

    jiaocheng

---
+++ devcom/src/main/java/com/bdqn/devcom/fragments/CommonFragment.java
@@ -0,0 +1,23 @@
+package com.bdqn.devcom.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bdqn.devcom.R;
+
+
+public class CommonFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_common, container, false);
+    }
+
+
+}
